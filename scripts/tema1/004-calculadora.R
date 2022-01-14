2fa432adf38923b930ec1c464a16fe8017a93765
Tomás Van Den Heuvel <tomasvdhcode@gmail.com>
2022-01-14 20:58:59 -0300

    Update 004-calculadora.R

--- scripts/tema1/004-calculadora.R
+++ scripts/tema1/004-calculadora.R
@@ -1,3 +1,5 @@
+# Funciones y Combinatoria
+
 2*(3+5/2)
 
 2*((3+5)/2)
@@ -69,3 +71,45 @@ factorial(5)
 factorial(0)
 
 choose(5,        3)
+
+
+
+# Funciones Trigonometricas
+    cos(60*pi/180)
+    sin(60*pi/180)
+    ?sin
+  
+    # sinpi(1/2) = sin(pi/2)
+    
+    tan(pi) # -1.224647e-16 ~ 0
+    tan(pi/2) # 1.633124e+16 ~ Inf
+    
+    asin(0.8660254) #Arc Sin en radianes
+    asin(0.8660254)*180/pi
+    
+    asin(5) #Arc sin x en [-1,1]
+    acos(-8)
+
+    
+    
+# Redondeo y decimales en R
+    print(sqrt(10),10)
+    round(sqrt(10),3)
+    floor(sqrt(10))    
+    ceiling(sqrt(10))    
+    trunc(sqrt(10))    
+
+    2^50
+    print(2^50,15)
+    print(2^50,2)
+    print(pi, 17)    
+    
+    round(sqrt(2), 4)^2
+    
+    round(1.25,1)    
+    round(1.35,1)
+    round(sqrt(2),1)
+    round(sqrt(2),0)
+    round(sqrt(2))
+    # ?round
+      
