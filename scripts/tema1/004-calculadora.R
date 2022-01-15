3cb6606312c5c86bc994c76782996f48f10e0375
Tomás Van Den Heuvel <tomasvdhcode@gmail.com>
2022-01-15 17:53:26 -0300

    Update 004-calculadora.R

--- scripts/tema1/004-calculadora.R
+++ scripts/tema1/004-calculadora.R
@@ -168,4 +168,50 @@
     
     # Remover todas las funciones o variables
     rm(list = ls())
-        
+
+    
+    
+# Numeros complejos
+    class(3+2i)
+    
+    (3+2i)*5    
+    (3+2i)*(-1+3i)
+    (3+2i)/(-1+3i)
+    
+    # pi + sqrt(2)i
+    complex(real = pi, imaginary = sqrt(2)) -> z1
+    z1
+
+    # sqrt(-5)
+    sqrt(as.complex(-5))
+    
+    # La raiz cuadrada devuelve, de las dos soluciones la de
+    # Re(z)>0, para obtener la otra, hay que multiplicar por -1
+    sqrt(3+2i) # z^2 = 3+2i
+    exp(3+2i)    
+    sin(3+2i)    
+    cos(3+2i)
+    
+    #Módulo = sqrt(Re(z)^2 + Im(z)^2)
+    Mod(z1)
+    
+    #Argumento = arctan(Im(z)/Re(z))
+    # = arccos(Re(z)/Mod(z))
+    # = arcsin(Im(z)/Mod(z))
+    # va desde (-pi,pi]
+    Arg(-1+0i)
+    Arg(-1+-2i)
+    
+    #Conjugado = Re(z) - Im(z)i
+    Conj(z1)
+    
+    #Parte real y parte imaginaria
+    Re(z1)
+    Im(z1)
+    
+    ### z = Mod(z) * (cos(Arg(z))+sin(Arg(z)))
+    complex(modulus = 2, argument = pi/2) -> z2
+    Mod(z2)
+    Arg(z2)
+    
+    
